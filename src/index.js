02c42cd893629c351d0313976ada3b9195bf733f
Étienne Brouillard <etienne.brouillard@outlook.com>
2018-08-01 10:10:40 -0400

    adding traces

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ const HumiditySensor = require("./humidity-sensor");
 const ImuSensor = require("./imu-sensor");
 const PressureSensor = require("./pressure-sensor");
 const UvSensor = require("./uv-sensor");
-
+const fs = require("fs");
 
 let config = null;
 try {
+    const rootFiles = fs.readdirSync("./");
+    rootFiles.forEach((element) => {
+        console.log(element);
+    });
+    const configFiles = fs.readdirSync("./config");
+    configFiles.forEach((element) => {
+        console.log(element);
+    });
     config = require("./config/config.json");
 } catch (error) {
+    console.error(error);
     console.log(`can't load config from folder, using dummy config`);
     config = {
         "interval": 60000,
